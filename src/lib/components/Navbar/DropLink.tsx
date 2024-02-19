1374a177c4f50b754ddb997505dbf9ae747fa637
Afanur Hasan <82591975+raza11272@users.noreply.github.com>
2024-02-19 13:23:36 +0600

    Create DropLink.tsx
    drop limnk

---
+++ src/lib/components/Navbar/DropLink.tsx
@@ -0,0 +1,33 @@
+import { setOpen } from "@/lib/redux/reducers/navbarSlice";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { DropdownMenuRadioItem } from "@/components/ui/dropdown-menu";
+
+
+
+type NavbarProps = {
+    name: String;
+    path: String;
+};
+
+const DropLink = ({ name, path }: NavbarProps) => {
+    const dispatch = useDispatch();
+
+    return (
+        <NavLink
+            //@ts-ignore
+            to={path}
+            onClick={() => dispatch(setOpen())}
+            className={`capitalize font-semibold hover:text-primary   ${({
+                //@ts-ignore
+                isActive,
+            }) => (isActive ? "active" : "")} `}
+        >
+            <DropdownMenuRadioItem className="list-none">
+                {name}
+            </DropdownMenuRadioItem>
+        </NavLink>
+    );
+};
+
+export default DropLink;
