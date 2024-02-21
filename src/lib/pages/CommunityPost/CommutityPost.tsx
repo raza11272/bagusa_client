0644fbb99f27f2445d39ec855f74f0a60bb4b297
Afanur Hasan <82591975+raza11272@users.noreply.github.com>
2024-02-21 14:10:42 +0600

    Update CommutityPost.tsx
    phon loock

--- src/lib/pages/CommunityPost/CommutityPost.tsx
+++ src/lib/pages/CommunityPost/CommutityPost.tsx
@@ -47,7 +47,7 @@ const CommutityPost = () => {
           subtitle={"see all posts from our Community"}
         />
       </div>
-      <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.newsPosts &&
         //@ts-ignore
           data.newsPosts.data.map((item, index) => (
